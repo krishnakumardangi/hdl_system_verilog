ea0e16194f6c888a0df6b2a8c21e63e9da46ad5c
Krishna <studyroom19471@gmail.com>
2023-12-16 17:41:35 +0530

    Adding a example of Verilog code

--- verilogExampleT1.v
+++ verilogExampleT1.v
@@ -2,7 +2,9 @@
  * Author: Krishna Kumar
  * File Name: verilogExampleT1.v
  * Objective: Writing a verilog example
- * Description: In this example, we can observed that last module get compile first and top module get compile at end (compile mean produce display first). And $monitor produce output at end of program and only one $monitor can be use in a verilog code. $monitor is used when we want to output due to change variables.
+ * Description: In this example, we can observed that last module get compile first and top module get compile at end (compile mean produce display first). 
+ * And $monitor produce output at end of program and only one $monitor can be use in a verilog code. 
+ * $monitor is used when we want to output due to change variables.
  * Date: 16-12-2023
  * Version: 1.0.01
  * Other files: Not required
@@ -10,12 +12,11 @@
  */
 
 module exampleUsingGate;
-    reg a, b, A, B;
-    wire c, C;
+    reg a, b;
+    wire c;
     initial begin a=1'b0; b=1'b1; end
     
     and g1 (c, a, b);
-    assign C = A & B;
     
     initial begin $monitor("This example module using AND Gate with monitor, here A = %b, B = %b and A.B = %b", a, b, c); end
     initial begin $display("This example module using AND Gate with display, here A = %b, B = %b and A.B = %b", a, b, c); end
