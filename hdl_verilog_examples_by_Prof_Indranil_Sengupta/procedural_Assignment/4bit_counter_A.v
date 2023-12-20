af6368b6c56e520e8b21b04aa125a64c2ab6f6e0
Krishna <studyroom19471@gmail.com>
2023-12-20 21:08:04 +0530

    4-bit counter with asynchronous reset

---
+++ hdl_verilog_examples_by_Prof_Indranil_Sengupta/procedural_Assignment/4bit_counter_A.v
@@ -0,0 +1,18 @@
+/*
+The	event	condition	triggers	
+when	either	a	positive	edge	of	
+“clk”	comes,	or	a	positive	edge	
+of	“rst”.	
+*/
+// 4-bit counter with asynchronous reset
+module counter (clk, rst, count);
+    input clk, rst;
+    output reg [3:0] count;
+    always @(posedge clk or posedge rst) 
+    begin
+        if (rst) 
+            count <= 0;
+        else
+            count <= count + 1;
+    end
+endmodule
