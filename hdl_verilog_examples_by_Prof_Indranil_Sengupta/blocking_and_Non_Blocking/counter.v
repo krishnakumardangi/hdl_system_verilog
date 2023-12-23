cec19f5eb0fa15eede48fa48d8899c962f039c91
Krishna <studyroom19471@gmail.com>
2023-12-23 17:13:34 +0530

    Example of counter using Non-Blocking Procedural Assignment

--- hdl_verilog_examples_by_Prof_Indranil_Sengupta/blocking_and_Non_Blocking/counter.v
+++ hdl_verilog_examples_by_Prof_Indranil_Sengupta/blocking_and_Non_Blocking/counter.v
@@ -7,13 +7,13 @@ module counter (mode, clr, ld, d_in, clk, count);
     always @ (posedge clk)
       begin
         if (ld)
-            count = d_in;
+            count <= d_in;
         else if (clr)
-            count = 0;
+            count <= 0;
         else if (mode)
-            count = count + 1;
+            count <= count + 1;
         else 
-            count = count - 1;
+            count <= count - 1;
       end
 endmodule
 
@@ -26,8 +26,10 @@ module counter_N ( clear, clock, count);
     output reg [0:N] count;
     
     always @ (negedge clock)
+      begin
         if (clear)
             count <= 0;
         else
             count <= count + 1;
+      end
 endmodule
